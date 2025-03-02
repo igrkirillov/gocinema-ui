54a402540b257d76220f3213d1401efddcd2f2f0
Igor.Kirillov <Igor.Kirillov@x5.ru>
2025-03-02 15:56:37 +0300

    memorized selector for movie selection

--- src/slices/buying/index.ts
+++ src/slices/buying/index.ts
@@ -29,7 +29,6 @@ export const buyingSlice = createSliceWithThunk({
         loadBuying: create.asyncThunk<{bookedPlaces: BookedPlace[], seance: Seance, seanceDate: string}, {seanceId: number, seanceDate: string}>(
             async  ({seanceId, seanceDate}, thunkApi) => {
                 try {
-                    console.log("seanceDate " + seanceDate)
                     const currentUser = getCurrentUser(thunkApi.getState());
                     return {
                         bookedPlaces: await getBookedPlaces(currentUser, seanceId, seanceDate),
