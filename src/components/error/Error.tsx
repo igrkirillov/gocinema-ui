2b6d657008daf8ab2396ffcc5a09be2261fb46c3
Igor.Kirillov <Igor.Kirillov@x5.ru>
2025-03-10 10:19:52 +0300

    фиксы замечаний после проверки 10 марта

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,6 +1,10 @@
+import styles from "../admin/css/styles.module.scss";
+
 export function Error(props: {error: string}) {
     const {error} = props;
     return (
-        <div><span><b>Ошибка:</b> {error + ""}</span></div>
+        <div className={styles["text-center"]}>
+            {error ? (<span className={styles["login__input"]} style={{"color": "red", "border": "0px"}}>{error}</span>) : null}
+        </div>
     )
 }
